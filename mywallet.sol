fbfa03dc04aa3c94b9092179cb5e7d53eafb1a45
shockflux <43464545+shockflux@users.noreply.github.com>
2021-07-18 01:40:12 +0530

    BASIC

---
+++ mywallet.sol
@@ -0,0 +1,1 @@
+pragma solidity ^0.5.0;contract MyContract {    mapping(address => uint256) public balances;    address payable wallet;    constructor(address payable _wallet) public {        wallet = _wallet;    }    function buyToken() public payable {        balances[msg.sender] += 1;        wallet.transfer(msg.value);    }}
